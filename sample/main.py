c0a03b60aeacc9633e80fedfe3194e20f1c32e2a
Qais Patankar <qaisjp@gmail.com>
2019-02-02 14:06:18 +0100

    Add intro message to sample

--- sample/main.py
+++ sample/main.py
@@ -4,6 +4,7 @@ import asyncio
 def main():
     r = Remote("35ae6830-d961-4a9c-937f-8aa5bc61d6a3", "ws://localhost:8080")
     r.add_callback(RPCType.MOVE_IN_DIRECTION, lambda direction: print("Moving in direction", direction))
+    print("Listening for updates...")
     asyncio.get_event_loop().run_until_complete(r.connect())
 
 if __name__ == "__main__":

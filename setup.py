e9a141f00fac37c6d9bcfd7cdfd5b2782c679ada
Qais Patankar <qaisjp@gmail.com>
2019-02-02 13:26:34 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='growbot',
+    version='0.1',
+    author="Team xiv",
+    author_email="me@qaisjp.com",
+    description="Client library for GrowBot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/teamxiv/growbot-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
+
+# To Publish:
+# First, build a source distribution:
+# $ python setup.py sdist
+# Then upload this to PyPi (have ~/.pypirc exist)
+# $ twine upload dist/*

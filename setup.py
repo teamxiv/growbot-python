06dd1c060bbb4ebaf646343414317932b98a40b9
Qais Patankar <qaisjp@gmail.com>
2019-02-05 23:44:12 +0000

    Update growbot-python to 0.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='growbot',
-    version='0.2.4',
+    version='0.2.5',
     author="Team xiv",
     author_email="me@qaisjp.com",
     description="Client library for GrowBot",
